ed826850cc0bcea3f0323b2de033905f1bd233fa
i-am-renbin <renbin98@petalmail.com>
2022-10-31 23:07:47 +0800

    酷玩页面

--- js/play.js
+++ js/play.js
@@ -1,25 +1,56 @@
 axios.defaults.headers = {
     'Content-Type': 'application/x-www-form-urlencoded'
 }
-const vv=new Vue({
-    el:'#app',
-    data:{
-        goods:[],
-        x:2,
-        page_:true
+const vv = new Vue({
+    el: '#app',
+    data: {
+        bars: [{ name: '最新', value: true }, { name: '最热', value: false }, { name: '分类', value: false }],
+        goods: [],
+        hotsGoods:[],
+        x: 2,
+        active: true,
+        tabs:[
+            {tab:'content1',value:true},
+            {tab:'content2',value:false},
+            {tab:'content3',value:false}
+        ]
     },
-    methods:{
-       async get01(){
-        let result=  await axios.get('http://127.0.0.1:3000/play/new')
-        this.goods= result=result.data;
-        
-           console.log(result);
+    methods: {
+        async get01() {
+            let result = await axios.get('http://127.0.0.1:3000/play/new')
+            this.goods = result = result.data;
+
+        },
+        async get02() {
+            let result = await axios.get('http://127.0.0.1:3000/play/hot')
+            this.hotsGoods = result = result.data;
+
+        },
+        change_() {
+            this.x = 4;
+        },
+        changeTab(item,index){
+            
+            for(let i =0;i<this.bars.length;i++){
+                this.bars[i].value=false;
+                this.tabs[i].value=false;
+            }
+            
+            item.value=!item.value
+            this.tabs[index].value=true;
+            if(index==2){
+                
+                this.x=4;
+            }else{
+                this.x=2;
+            }
         }
     },
-    mounted(){
+    mounted() {
         this.get01();
+        this.get02();
     },
-    computed:{
+    computed: {
         // async getData(){
         // let result= await axios.get('http://127.0.0.1:3000/play/new')
         //     this.goods=result.data;
@@ -27,6 +58,3 @@ const vv=new Vue({
         // }
     }
 });
-
-// let ss=axios.get('http://127.0.0.1:3000/play/new');
-// console.log(ss);
